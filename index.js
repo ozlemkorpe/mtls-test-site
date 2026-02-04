f6fc37d9f093a7fed126a856d9a890b0d21a29c9
Ozlem Korpe <25618760+ozlemkorpe@users.noreply.github.com>
2026-02-04 11:45:41 +0300

    Form auth implemented

--- index.js
+++ index.js
@@ -1,30 +1,69 @@
 const https = require("https");
 const fs = require("fs");
 const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
+// ---------- Middlewares ----------
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "mtls-test-secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// ---------- TLS Options ----------
 const options = {
   key: fs.readFileSync("./certs/server.key"),
   cert: fs.readFileSync("./certs/server.crt"),
   ca: fs.readFileSync("./certs/ca.crt"),
   requestCert: true,
-  rejectUnauthorized: true
+  rejectUnauthorized: true,
 };
 
-app.get("/", (req, res) => {
+// ---------- Guards ----------
+function mtlsRequired(req, res, next) {
   if (!req.client.authorized) {
-    return res.status(401).send("Client certificate required");
+    return res.status(401).send("❌ Client certificate required");
   }
+  next();
+}
 
-  const cert = req.socket.getPeerCertificate();
+function loginRequired(req, res, next) {
+  if (req.session.authenticated) {
+    return next();
+  }
+  res.redirect("/login");
+}
 
-  res.send(`
-    <h1>✅ mTLS Test Site</h1>
-    <pre>${JSON.stringify(cert.subject, null, 2)}</pre>
-  `);
+// ---------- Routes ----------
+app.get("/login", mtlsRequired, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/login.html"));
 });
 
+app.post("/login", mtlsRequired, (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === "test" && password === "test123") {
+    req.session.authenticated = true;
+    return res.redirect("/");
+  }
+
+  res.redirect("/login?error=1");
+});
+
+app.get("/", mtlsRequired, loginRequired, (req, res) => {
+  const cert = req.socket.getPeerCertificate();
+  res.sendFile(path.join(__dirname, "views/index.html"));
+});
+
+// ---------- Server ----------
 https.createServer(options, app).listen(443, () => {
-  console.log("🚀 HTTPS mTLS server running on 443");
+  console.log("🚀 HTTPS mTLS server running on https://localhost");
 });
